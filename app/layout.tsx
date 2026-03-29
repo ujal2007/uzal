af7bdfd90ccc88d09d84520375dbc586684bee81
ujal2007 <201.00023.13.0045@education.gov.bt>
2026-03-28 19:22:06 -1200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "UZAL.fx - All Social Links",
-  description: "Connect with UZAL on all platforms - TikTok, Instagram, Twitter, YouTube and more",
+  description: "welcome its UZAL here!! - TikTok, Instagram, Twitter, YouTube and more",
   generator: "UZAL.DEV",
 }
 
